fb61cbab5539a82ccf756b0d886aab4b47e73033
anishka07 <anishkamukherjee237@gmail.com>
2025-04-25 15:05:08 +0545

    instance call update

--- examples/nepali_rag_example.py
+++ examples/nepali_rag_example.py
@@ -1,24 +1,24 @@
-import re 
 import time
+import json
 
 from src.services.rag_main import NepaliLawRAG
 from src.utils.settings import ConstantSettings, PathSettings
-
 
 if __name__ == "__main__":
     start_time = time.time()
     r = NepaliLawRAG(
         chunk_size=ConstantSettings.CHUNK_SIZE,
-        chunk_overlap=ConstantSettings.CHUNK_OVERLAP,
         model_name=ConstantSettings.EMBEDDING_MODEL,
+        chunk_overlap=ConstantSettings.CHUNK_OVERLAP,
+        use_heavy=True
     )
 
-    document_paths = [PathSettings.PDF_DIR / "example1.pdf"]
+    document_paths = [PathSettings.PDF_DIR / "monopoly.pdf", PathSettings.PDF_DIR / "example1.pdf"]
 
-    extracted_text = r.extract_text_from_documents(document_list=document_paths)
-    a = list(extracted_text.values())
-    b = a[0]
-    b = re.sub(r'\.{2,}', '', b)
-    print(b)
+    a = r.process_documents(document_paths=document_paths)
+    # with open("eg.json", "w") as f:
+    #     json.dump(a, f, indent=4)
+    # print("done")
+    print(a['example1']['chunks'])
 
     print(f"Total time taken: {time.time() - start_time}")
