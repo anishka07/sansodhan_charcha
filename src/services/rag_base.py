20e6292196fa1167f6de51d9214f72b49ad976ae
anishka07 <anishkamukherjee237@gmail.com>
2025-04-25 15:04:48 +0545

    added a feature to load and save cache

--- src/services/rag_base.py
+++ src/services/rag_base.py
@@ -1,22 +1,20 @@
-import re 
+import re
 import os
 import pickle
 from pathlib import Path
 from threading import Lock
-from typing import List, Dict
+from typing import List, Dict, Optional
 
 import fitz
 import numpy as np
 from sentence_transformers import SentenceTransformer
-from chromadb import Settings, Client
 
 from src.repo.chroma_client import ChromaClientSingleton
 from src.utils.logger import get_custom_logger
-from src.utils.settings import PathSettings, ConstantSettings
+from src.utils.settings import PathSettings
 
 
 class STSingleton:
-
     _instance = None
     _lock: Lock = Lock()
 
@@ -26,62 +24,65 @@ class STSingleton:
                 cls._instance = super(STSingleton, cls).__new__(cls)
                 cls._instance._initialize(model_name)
         return cls._instance
-        
+
     def _initialize(self, model_name: str):
         self.model_name = model_name
         self.model = SentenceTransformer(model_name)
-    
+
     def get_model(self):
         return self.model
 
 
 class NepaliRAGBase:
 
-    def __init__(self, chunk_size: int, model_name: str):
+    def __init__(self, chunk_size: int, model_name: str, use_heavy: bool = True):
         self.chunk_size = chunk_size
         self.save_dir_path = PathSettings.CACHE_DIR
         self.logger = get_custom_logger(name="Sansodhan Charcha")
-        self.logger.info("Initializing ChromaDB client and SentenceTransformer model...")
-        self.embeddings_gen_instance = STSingleton(model_name).get_model()
-        # self.embeddings_gen_instance = SentenceTransformer(ConstantSettings.EMBEDDING_MODEL)
-        # self.chroma_client = Client(Settings(persist_directory=str(PathSettings.CHROMA_DIR)))
-        self.chroma_client = ChromaClientSingleton().get_client()
-        self.logger.info("ChromaDB client and SentenceTransformer model initialized successfully.")
-    
-    def get_cache_path(self, doc_name: str):
-        cache_path = self.save_dir_path / f"{doc_name}.pkl"
+        # remove this 
+        if use_heavy:
+            self.logger.info("Initializing ChromaDB client and SentenceTransformer model...")
+            self.embeddings_gen_instance = STSingleton(model_name).get_model()
+            self.chroma_client = ChromaClientSingleton().get_client()
+            self.logger.info("ChromaDB client and SentenceTransformer model initialized successfully.")
+
+    def _get_cache_path(self, doc_name: str, suffix: str = ""):
+        cache_path = self.save_dir_path / f"{doc_name}{f'_{suffix if suffix else ''}'}.pkl"
         return cache_path
-    
+
+    def _load_cache(self, doc_name: str, suffix: str = ""):
+        cache_path = self._get_cache_path(doc_name, suffix)
+        if os.path.exists(cache_path):
+            self.logger.info(f"Loading cache from {cache_path}")
+            with open(cache_path, "rb") as f:
+                return pickle.load(f)
+        return None
+
+    def _save_cache(self, doc_name: str, data: object, suffix: str = ""):
+        cache_path = self._get_cache_path(doc_name, suffix)
+        cache_path.parent.mkdir(parents=True, exist_ok=True)
+        if not os.path.exists(cache_path):
+            with open(cache_path, "wb") as f:
+                pickle.dump(data, f)
+            self.logger.info(f"Saving to {cache_path}")
+
     @staticmethod
     def _get_doc_name(document_path: Path):
         doc_name = str(document_path).split("/")[-1]
         doc_name = doc_name.split(".")[0]
         return doc_name
-    
-    def _save_cache_as_pkl(self, doc_name: str, data: object):
-        cache_path = self.get_cache_path(doc_name)
-        os.makedirs(cache_path.parent, exist_ok=True)
-        with open(cache_path, "wb") as f:
-            pickle.dump(data, f)
 
-    def _load_cache_from_pkl(self, doc_name: str):
-        cache_path = self.get_cache_path(doc_name)
-        if os.path.exists(cache_path):
-            with open(cache_path, "rb") as f:
-                return pickle.load(f)
-        return None
-
-    def extract_text_from_documents(self, document_list: list[Path]):
+    def extract_text_from_documents(self, document_list: list[Path]) -> Dict[str, Optional[str]]:
         results = {}
         for document_path in document_list:
             doc_name = self._get_doc_name(document_path)
-            cache_data = self._load_cache_from_pkl(doc_name)
+            cache_data = self._load_cache(doc_name, "text")
 
-            if cache_data:
+            if cache_data is not None:
                 self.logger.info(f"Loading cache for {doc_name}")
                 results[doc_name] = cache_data
                 continue
-            
+
             extracted_text = ""
             if document_path.suffix.lower() == ".pdf":
                 try:
@@ -91,22 +92,21 @@ class NepaliRAGBase:
                             extracted_text = " ".join(extracted_text.split())
                             extracted_text = re.sub(r'\.{2,}', '', extracted_text)
                     self.logger.info(f"Saving cache for {doc_name}")
-                    self._save_cache_as_pkl(doc_name, extracted_text)
+                    self._save_cache(doc_name, extracted_text, "text")
                     results[doc_name] = extracted_text
                 except Exception as e:
                     self.logger.error(f"Error reading PDF {doc_name}: {e}")
                     results[doc_name] = ""
         return results
-    
+
     def chunk_text(self, text: str) -> list[str]:
         raise NotImplementedError("Chunking method not implemented.")
-    
+
     def embed_text(self, chunked_text: list[str]) -> np.ndarray:
         raise NotImplementedError("Embedding method not implemented.")
-    
+
     def save_embeddings(self, chunks: List[str], embeddings: np.ndarray, metadata: List[Dict]) -> str:
         raise NotImplementedError("Saving embeddings method not implemented.")
-    
+
     def retrieve_results(self, query: str, top_k: int):
         raise NotImplementedError("Retrieving results method not implemented.")
-    
--- src/services/rag_main.py
+++ src/services/rag_main.py
@@ -14,80 +14,94 @@ class NepaliLawRAG(NepaliRAGBase):
             self,
             model_name: str,
             chunk_size: int,
+            use_heavy: bool,
             chunk_overlap: int,
             collection_name: str = None
     ):
-        super().__init__(chunk_size, model_name)
+        super().__init__(chunk_size, model_name, use_heavy)
         self.chunk_overlap = chunk_overlap
-        if collection_name is None:
-            self.collection_name = "law_collection"
-        self.collection = self.chroma_client.get_or_create_collection(
-            name=self.collection_name,
-            metadata={"description": "Nepali law amendments and legal documents"},
-        )
-        self.logger.info(f"Chromadb collection {self.collection_name} initialized.")
+        # remove this 
+        if use_heavy:
+            if collection_name is None:
+                self.collection_name = "law_chroma_collection"        
+            self.collection = self.chroma_client.get_or_create_collection(
+                name=self.collection_name,
+                metadata={"description": "Nepali law amendments and legal documents"},
+            )
+            self.logger.info(f"Chroma db collection {self.collection_name} initialized.")
 
         self.text_splitter = RecursiveCharacterTextSplitter(
             chunk_size=self.chunk_size,
             chunk_overlap=self.chunk_overlap,
             length_function=len,
-            separators=["\n\n", "\n", " ", ""]
+            separators=[
+                "\n\n",
+                "\n",
+                " ",
+                ".",
+                ",",
+                "\u200b",  # Zero-width space
+                "\uff0c",  # Fullwidth comma
+                "\u3001",  # Ideographic comma
+                "\uff0e",  # Fullwidth full stop
+                "\u3002",  # Ideographic full stop
+                "",
+            ]
         )
 
-    def process_documents(
-            self,
-            document_path: List[Path]
-    ) -> Dict[str, Dict[str, Any]]:
+    def process_documents(self, document_paths: List[Path]):
+        self.logger.info(f"Processing {len(document_paths)} documents...")
+        extracted_text = self.extract_text_from_documents(document_paths)
 
-        self.logger.info(f"Processing {len(document_path)} documents...")
-
-        document_texts = self.extract_text_from_documents(document_path)
         results = {}
-        for doc_name, text in document_texts.items():
+        for doc_name, text in extracted_text.items():
             if not text:
-                self.logger.warning(f"No text found in {doc_name}. Skipping...")
+                self.logger.warning(f"Document {doc_name} has no text. Skipping.")
                 continue
             self.logger.info(f"Processing text for {doc_name}...")
+            # Loading existing chunks, embeddings and chunk metadata from pickle files
+            chunks = self._load_cache(doc_name, "chunks")
+            embeddings = self._load_cache(doc_name, "embeddings")
+            chunk_metadata = self._load_cache(doc_name, "metadata")
 
-            chunks = self.chunk_text(text)
-
-            chunk_metadata = []
-            for i, _ in enumerate(chunks):
-                metadata = {
+            if chunks is not None and embeddings is not None and chunk_metadata is not None:
+                self.logger.info("Using cached chunks, embeddings and metadata.")
+            else:
+                chunks = self.chunk_text(text)
+                chunk_metadata = [{
                     "doc_name": doc_name,
                     "chunk_id": i,
                     "source": f"{doc_name}_{i}_chunk"
-                }
-                chunk_metadata.append(metadata)
+                } for i, _ in enumerate(chunks)]
 
-            emebeddings = self.embed_text(chunks)
+                embeddings = self.embed_text(chunks)
+                # Saving the chunks, embeddings and chunk metadata as pickle files
+                self._save_cache(doc_name, chunks, "chunks")
+                self._save_cache(doc_name, embeddings, "embeddings")
+                self._save_cache(doc_name, chunk_metadata, "metadata")
 
-            doc_id = self.save_embeddings(chunks, emebeddings, chunk_metadata)
+            doc_id = self.save_embeddings(chunks, embeddings, chunk_metadata)
             results[doc_name] = {
-                "text_length": len(text),
+                "text_len": len(text),
                 "chunks": chunks,
                 "doc_id": doc_id
             }
-        self.logger.info(f"Successfully processed {len(results)} documents")
+        self.logger.info(f"Finished processing {len(results)} documents.")
         return results
 
     def chunk_text(self, text: str) -> List[str]:
-        self.logger.info(f"Chunking text of length {len(text)}...")
+        self.logger.info(f"Chunking text of {len(text)}...")
         chunks = self.text_splitter.split_text(text)
-        self.logger.info(f"Text chunked into {len(chunks)} chunks.")
+        self.logger.info(f"Chunked text in {len(chunks)} chunks...")
         return chunks
 
-    def embed_text(self, chunked_text: List[str]) -> np.ndarray:
-        self.logger.info(f"Generating embeddings for {len(chunked_text)} chunks...")
-        embeddings = []
-        for chunk in tqdm(chunked_text, desc="Embedding chunks"):
-            embedding = self.embeddings_gen_instance.encode(chunk)
-            embeddings.append(embedding)
-
+    def embed_text(self, chunked_text: list[str]) -> np.ndarray:
+        self.logger.info(f"Embedding text of {len(chunked_text)}...")
+        embeddings = [self.embeddings_gen_instance.encode(chunk) for chunk in tqdm(chunked_text, desc="Embedding text")]
         return np.array(embeddings)
 
     def save_embeddings(self, chunks: List[str], embeddings: np.ndarray, metadata: List[Dict]) -> str:
-        self.logger.info(f"Saving {len(chunks)} chunks to ChromaDB")
+        self.logger.info(f"Saving embeddings of {len(chunks)} chunks...")
 
         import uuid
         batch_id = str(uuid.uuid4())
@@ -102,41 +116,35 @@ class NepaliLawRAG(NepaliRAGBase):
             metadatas=metadata,
             ids=ids
         )
-
         self.logger.info(f"Successfully saved embeddings with batch ID: {batch_id}")
         return batch_id
 
     def retrieve_results(self, query: str, top_k: int = 5):
-        self.logger.info(f"Searching for {query} in ChromaDB...")
-        query_embedding = self.embeddings_gen_instance.encode(query)
+        self.logger.info(f"Retrieving results for {query}...")
+        query_em = self.embeddings_gen_instance.encode(query)
         results = self.collection.query(
-            query_embeddings=[query_embedding.tolist()],
+            query_embeddings=[query_em.tolist()],
             n_results=top_k,
             include=["documents", "metadatas", "distances"]
         )
-
-        formatted_results = []
-        for i in range(len(results["documents"][0])):
-            formatted_results.append({
-                "text": results["documents"][0][i],
-                "metadata": results["metadatas"][0][i],
-                "similarity": 1 - results["distances"][0][i]
-            })
-        self.logger.info(f"Found {len(formatted_results)} results")
+        formatted_results = [{
+            "text": results["documents"][0][i],
+            "metadata": results["metadatas"][0][i],
+            "similarity": 1 - results["distances"][0][i]
+            }
+            for i in range(len(results["documents"][0]))]
         return formatted_results
 
-    def get_context_for_query(self, query: str, top_k: int = 5) -> str:
+    def get_context_for_query(self, query: str, top_k: int = 5):
         results = self.retrieve_results(query, top_k)
 
         results.sort(key=lambda x: x["similarity"], reverse=True)
-
         context = "\n\n---\n\n".join([
             f"Source: {r['metadata']['doc_name']}\n"
             f"Similarity: {r['similarity']:.2f}\n"
             f"Content: {r['text']}"
             for r in results
         ])
-
         return context
 
     def search_law_amendments(self, query: str, top_k: int = 5) -> Dict[str, Any]:
